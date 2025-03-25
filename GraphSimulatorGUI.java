1d61b26a81eb9de80aa73033f00f4415da4754f5
Aadi <aadijain1903@gmail.com>
2025-03-25 22:19:19 +0530

    Implemented deadlock detection using cycle detection

--- GraphSimulatorGUI.java
+++ GraphSimulatorGUI.java
@@ -1,11 +1,23 @@
-// Basic GUI Setup
 import javax.swing.*;
+import java.awt.*;
 
 public class GraphSimulatorGUI {
     public GraphSimulatorGUI() {
         JFrame frame = new JFrame("Resource Allocation Graph Simulator");
         frame.setSize(600, 400);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        // Set layout manager
+        frame.setLayout(new FlowLayout());
+
+        // Add buttons for user interaction
+        JButton addProcessBtn = new JButton("Add Process");
+        JButton addResourceBtn = new JButton("Add Resource");
+
+        frame.add(addProcessBtn);
+        frame.add(addResourceBtn);
+
+        // Set visibility after adding components
         frame.setVisible(true);
     }
 
